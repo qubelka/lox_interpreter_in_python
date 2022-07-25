782d7e4427ed14ddc8d0ae6facc7e974471e855a
Qubelka <42945433+qubelka@users.noreply.github.com>
2022-07-25 14:11:01 +0200

    Fix tests

--- src/test_interpreter.py
+++ src/test_interpreter.py
@@ -14,141 +14,141 @@ class TestInterpreter(unittest.TestCase):
         return interpreter
 
     def test_expression0(self):
-        interpreter = self.makeInterpreter("1")
+        interpreter = self.makeInterpreter("1;")
         result = interpreter.interpret()
         self.assertEqual(result, 1)
 
     def test_expression1(self):
-        interpreter = self.makeInterpreter("1 + 1")
+        interpreter = self.makeInterpreter("1 + 1;")
         result = interpreter.interpret()
         self.assertEqual(result, 2)
 
     def test_expression2(self):
-        interpreter = self.makeInterpreter("-1 + 1")
+        interpreter = self.makeInterpreter("-1 + 1;")
         result = interpreter.interpret()
         self.assertEqual(result, 0)
 
     def test_expression3(self):
-        interpreter = self.makeInterpreter("1 + (-1)")
+        interpreter = self.makeInterpreter("1 + (-1);")
         result = interpreter.interpret()
         self.assertEqual(result, 0)
 
     def test_expression4(self):
-        interpreter = self.makeInterpreter("1 - 1")
+        interpreter = self.makeInterpreter("1 - 1;")
         result = interpreter.interpret()
         self.assertEqual(result, 0)
 
     def test_expression5(self):
-        interpreter = self.makeInterpreter("1 - (-1)")
+        interpreter = self.makeInterpreter("1 - (-1);")
         result = interpreter.interpret()
         self.assertEqual(result, 2)
 
     def test_expression6(self):
-        interpreter = self.makeInterpreter("-1 - 1")
+        interpreter = self.makeInterpreter("-1 - 1;")
         result = interpreter.interpret()
         self.assertEqual(result, -2)
 
     def test_expression7(self):
-        interpreter = self.makeInterpreter("-1 - (-1)")
+        interpreter = self.makeInterpreter("-1 - (-1);")
         result = interpreter.interpret()
         self.assertEqual(result, 0)
 
     def test_expression8(self):
-        interpreter = self.makeInterpreter("1 * 1")
+        interpreter = self.makeInterpreter("1 * 1;")
         result = interpreter.interpret()
         self.assertEqual(result, 1)
 
     def test_expression9(self):
-        interpreter = self.makeInterpreter("-1 * 1")
+        interpreter = self.makeInterpreter("-1 * 1;")
         result = interpreter.interpret()
         self.assertEqual(result, -1)
 
     def test_expression10(self):
-        interpreter = self.makeInterpreter("1 * (-1)")
+        interpreter = self.makeInterpreter("1 * (-1);")
         result = interpreter.interpret()
         self.assertEqual(result, -1)
 
     def test_expression11(self):
-        interpreter = self.makeInterpreter("-1 * (-1)")
+        interpreter = self.makeInterpreter("-1 * (-1);")
         result = interpreter.interpret()
         self.assertEqual(result, 1)
 
     def test_expression12(self):
-        interpreter = self.makeInterpreter("1 * 1 * -1 * 2 * -2")
+        interpreter = self.makeInterpreter("1 * 1 * -1 * 2 * -2;")
         result = interpreter.interpret()
         self.assertEqual(result, 4)
 
     def test_expression13(self):
-        interpreter = self.makeInterpreter("10 / 5")
+        interpreter = self.makeInterpreter("10 / 5;")
         result = interpreter.interpret()
         self.assertEqual(result, 2)
 
     def test_expression14(self):
-        interpreter = self.makeInterpreter("-10 / 5")
+        interpreter = self.makeInterpreter("-10 / 5;")
         result = interpreter.interpret()
         self.assertEqual(result, -2)
 
     def test_expression15(self):
-        interpreter = self.makeInterpreter("10 / (-5)")
+        interpreter = self.makeInterpreter("10 / (-5);")
         result = interpreter.interpret()
         self.assertEqual(result, -2)
 
     def test_expression16(self):
-        interpreter = self.makeInterpreter("-10 / (-5)")
+        interpreter = self.makeInterpreter("-10 / (-5);")
         result = interpreter.interpret()
         self.assertEqual(result, 2)
 
     def test_expression17(self):
-        interpreter = self.makeInterpreter("1 + 1 / 2")
+        interpreter = self.makeInterpreter("1 + 1 / 2;")
         result = interpreter.interpret()
         self.assertEqual(result, 1.5)
 
     def test_expression18(self):
-        interpreter = self.makeInterpreter("(1 + 1) / 2")
+        interpreter = self.makeInterpreter("(1 + 1) / 2;")
         result = interpreter.interpret()
         self.assertEqual(result, 1)
 
     def test_expression19(self):
-        interpreter = self.makeInterpreter("-1")
+        interpreter = self.makeInterpreter("-1;")
         result = interpreter.interpret()
         self.assertEqual(result, -1)
 
     def test_expression20(self):
-        interpreter = self.makeInterpreter("--1")
+        interpreter = self.makeInterpreter("--1;")
         result = interpreter.interpret()
         self.assertEqual(result, 1)
 
     def test_expression21(self):
-        interpreter = self.makeInterpreter("-(1 + 2 / 2)")
+        interpreter = self.makeInterpreter("-(1 + 2 / 2);")
         result = interpreter.interpret()
         self.assertEqual(result, -2)
 
     def test_expression22(self):
-        interpreter = self.makeInterpreter("1 * 2 - 3 / -5 + 0.8")
+        interpreter = self.makeInterpreter("1 * 2 - 3 / -5 + 0.8;")
         result = interpreter.interpret()
         self.assertEqual(result, 3.4000000000000004)
 
     # crafting interpreters 6.1
     def test_expression23(self):
-        interpreter = self.makeInterpreter("0.1 * (0.2 * 0.3)")
+        interpreter = self.makeInterpreter("0.1 * (0.2 * 0.3);")
         result = interpreter.interpret()
         self.assertEqual(result, 0.006)
 
     # crafting interpreters 6.1
     def test_expression24(self):
-        interpreter = self.makeInterpreter("(0.1 * 0.2) * 0.3")
+        interpreter = self.makeInterpreter("(0.1 * 0.2) * 0.3;")
         result = interpreter.interpret()
         self.assertEqual(result, 0.006000000000000001)
 
     def test_expression_division_by_zero(self):
-        interpreter = self.makeInterpreter("2 / 0")
+        interpreter = self.makeInterpreter("2 / 0;")
         with self.assertRaises(RTError):
             interpreter.interpret()
 
     def test_no_expression(self):
         interpreter = self.makeInterpreter("             ")
-        with self.assertRaises(InvalidSyntaxError):
-            interpreter.interpret()
+        result = interpreter.interpret()
+        self.assertEqual(result, None)
 
     def test_expression_invalid_syntax1(self):
         interpreter = self.makeInterpreter("1 +")
@@ -156,6 +156,7 @@ class TestInterpreter(unittest.TestCase):
             interpreter.interpret()
 
     def test_expression_invalid_syntax2(self):
+        # Prints wrong error msg --> update parser
         interpreter = self.makeInterpreter("1 1 1 -")
         with self.assertRaises(InvalidSyntaxError):
             interpreter.interpret()
@@ -176,7 +177,7 @@ class TestInterpreter(unittest.TestCase):
             interpreter.interpret()
 
     def test_expression_invalid_syntax6(self):
-        interpreter = self.makeInterpreter("(1 + 1")
+        interpreter = self.makeInterpreter("(1 + 1;")
         with self.assertRaises(InvalidSyntaxError):
             interpreter.interpret()
 
